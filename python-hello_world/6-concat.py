fe355e5fa731d0a1c395395c845f1b2f5751ec74
ofest <dars20@hotmail.com>
2025-09-02 15:00:27 +0200

    Feat: concat the message.

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# The strings to take
 str1 = "Holberton"
 str2 = "School"
-# This prints a welcome message
-print(f"Welcome to {str1} {str2}!")
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
