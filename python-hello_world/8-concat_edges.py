5a666f9031ac56163b7ac61a13f3b68cf5a26b32
ofest <dars20@hotmail.com>
2025-09-02 14:48:51 +0200

    Feat: Concat edges of the sentence.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-
-print(f"{str[39:66]} with Python")
+# This prints the string taked and the sentence added
+print(str[39:66] + " with Python")
