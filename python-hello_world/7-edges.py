493c330d76839fc36fe60c587b079476305143bf
ofest <dars20@hotmail.com>
2025-09-02 14:47:28 +0200

    Feat: contain differenst parts of the word

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
+# This prints the first 3 letters
 print(f"First 3 letters: {word[:3]}")
+# This prints the last 2 letters
 print(f"Last 2 letters: {word[-2:]}")
+# This prints the middle word
 print(f"Middle word: {word[1:-1]}")
-
-
-
