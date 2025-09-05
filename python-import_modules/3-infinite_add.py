37448d5031a53eaa5fd282882cfbbf9c547d2797
ofest <dars20@hotmail.com>
2025-09-05 14:45:00 +0200

    Feat: prints the result of the addition of all arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    
+    argc = len(sys.argv) - 1
+    sum = 0
+    for i in range(argc):
+        sum += int(sys.argv[i + 1])
+    print(sum)
