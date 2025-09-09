92f08bba60eab8c5e7d8f1dbe057a98bced81f97
ofest <dars20@hotmail.com>
2025-09-09 14:52:30 +0200

    Feat:  returns a tuple with the length of a string and its first character.

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return None
-    else:
+        if sentence == "":
+              return (len(sentence), None)
         return (len(sentence), sentence[0])
