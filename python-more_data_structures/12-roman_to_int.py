f01b8b81bcc5c10211ef53707f83b5e15fee186a
ofest <dars20@hotmail.com>
2025-09-10 16:11:43 +0200

    Feat: print roman numbers.

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None:
+    if roman_string is None:
         return 0
-    roman_dir  = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_dir = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    for i in range (len(roman_string)):
-        current = roman_dir[roman_string[i]]
+    for i in range(len(roman_string)):
+        current = rom_dir[roman_string[i]]
         if i + 1 < len(roman_string):
-            next_val = roman_dir[roman_string[i + 1]]
+            next_val = rom_dir[roman_string[i + 1]]
         if i + 1 < len(roman_string) and current < next_val:
             total -= current
         else:
