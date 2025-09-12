a6d467ee2d727b65f9dd5c1b491f4babe110daca
ofest <dars20@hotmail.com>
2025-09-12 21:50:38 +0200

    Feat:  function that adds 2 integers.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Module contains a function that adds 2 integers"""
+"""Module that contains a function to add two integers.
+"""
 
 
 def add_integer(a, b=98):
-    """Function that adds 2 integers
-    integers (a)
-    integers (b)
-    return (a + b)
+    """Return the sum of a and b.
+
+    Args:
+        a: int or float.
+        b: int or float, default is 98.
+
+    Raises:
+        TypeError: If a or b are not int or float.
+
+    Returns:
+        int: The sum of a and b after casting floats to int.
     """
-    if isinstance(a, (int, float)) is False:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)) is False:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a) 
-    b = int(b)
-    return a + b
+
+    return int(a) + int(b)
