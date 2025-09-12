99c39744b39ee62ac4f6e08c520d681df88e3d83
ofest <dars20@hotmail.com>
2025-09-12 13:04:20 +0200

    Feat: function that prints x elements of a list.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
+    count = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-        except:
+            count += 1
+        except (IndexError):
             break
-        i += 1
     print("")
-    return (i)
+    return (count)
