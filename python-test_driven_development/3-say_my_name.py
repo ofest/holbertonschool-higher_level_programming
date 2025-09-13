774bb17621cb2810299555a7d20f142eac398e62
ofest <dars20@hotmail.com>
2025-09-13 12:17:17 +0200

    Feat: Write a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Module contains a function that prints
-    My name is {first_name}{last_name}
-    TypeError exception with the message first_name must be a string or last_name must be a string
-"""
+"""Module contains function that prints full name and last name"""
+
+
 def say_my_name(first_name, last_name=""):
-    """
-    Function that prints, first_name and last name
-    """
-    if isinstance(first_name, str) is False:
+    """Prints formatted full name"""
+    if type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if isinstance(last_name, str) is False:
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
+    
