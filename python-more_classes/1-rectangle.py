fd6b1914916ec2df5944c009f1f8c15bdf50b9f4
ofest <dars20@hotmail.com>
2025-09-16 16:16:08 +0200

    Feat: Rectangle that defines a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Heigth setter"""
+        """Height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
