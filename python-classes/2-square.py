ba4252ad1ede9415c012d2ebb7591c02268b7736
ofest <dars20@hotmail.com>
2025-09-16 14:08:34 +0200

    Feat: empty class Square that defines a square adding size with a value and a TypeError.

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,14 +4,12 @@
 
 class Square:
     """Represent a Square"""
+    
     def __init__(self, size=0):
         """Initialize the private instance attribute: size
         Raise the TypeError or ValueError according to given condition"""
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer.")
         elif size < 0:
             raise ValueError("size must be >= 0.")
-
-    def get_size(self):
-        return self.__size
+        self.__size = size
