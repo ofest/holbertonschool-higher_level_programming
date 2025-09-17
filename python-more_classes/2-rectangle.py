d710a753c5c4644627c0f5c16ef3c5f664820347
ofest <dars20@hotmail.com>
2025-09-17 09:54:51 +0200

    Feat: Rectangle that defines the Area and Perimeter.

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -3,6 +3,8 @@
 
 
 class Rectangle:
+    """Represents a rectangle"""
+    
     def __init__(self, width=0, height=0):
         """Defines a rectangle by width and height"""
         self.width = width
