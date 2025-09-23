2be02da756577ca520670e7b5ebf4c9f7409be5f
ofest <dars20@hotmail.com>
2025-09-23 18:55:49 +0200

    Feat: Class base geometry with area and an exeption.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module contains a baseGeometry class"""
+
+
+class BaseGeometry():
+    """Public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
