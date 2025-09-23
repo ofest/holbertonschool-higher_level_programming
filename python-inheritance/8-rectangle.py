136ec280f89ea7314f85c9f14eca2386275471e9
ofest <dars20@hotmail.com>
2025-09-23 20:41:15 +0200

    Feat: Class base geometry with the area and and an subclass 'rectancle'.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,21 +9,16 @@ class BaseGeometry():
 
     """Public instance method that validates the value"""
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
+    """Represents a rectangle using attributes from  BaseGeometry."""
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
