a49309dcf792f81df94cdf5a4e91dafaf5f4d583
ofest <dars20@hotmail.com>
2025-09-23 18:54:20 +0200

    Feat: function that check the inherits form.

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module contains a function that checks the inherits form"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False."""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
