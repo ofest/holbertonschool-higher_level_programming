dbccac68e17153cc5a87b1496df696dc7ebadb2f
ofest <dars20@hotmail.com>
2025-09-24 12:04:40 +0200

    Feat: Class base geometry with area and integer validator.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,14 +3,16 @@
 
 
 class BaseGeometry:
-    """Defines methods for BaseGeomatery"""
+    """Defines methods for BaseGeometry"""
     def area(self):
         """Defines area of the object"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validates the value as an integer"""
-        if type(value) is not int:
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if isinstance(value, bool):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
