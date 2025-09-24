eac9e0456bb31e441f8f1adcd28102159013aa35
ofest <dars20@hotmail.com>
2025-09-24 11:44:25 +0200

    Feat: Class base geometry with the area and and an subclass 'rectancle'.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-""" Module contains a baseGeometry class"""
+""" Module defines a Rectangle class that inherits from BaseGeometry """
 
 
-class BaseGeometry():
-    """Public instance method"""
-    def area(self):
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    """Public instance method that validates the value"""
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+""" Module defines a Rectangle class that inherits from BaseGeometry """
 
 
 class Rectangle(BaseGeometry):
