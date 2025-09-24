7851de924f84198d6c9572d7df3437ad8ff5eb08
ofest <dars20@hotmail.com>
2025-09-24 10:24:53 +0200

    Feat: class Rectangle that inherits from BaseGeometry

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Module contains a baseGeometry class"""
+
+
+class BaseGeometry():
+    """Public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """Public instance method that validates the value"""
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle using attributes from  BaseGeometry."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
