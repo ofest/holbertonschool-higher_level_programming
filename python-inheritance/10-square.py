be7bbf4698c2732120814db39d7bfb02e8c4b126
ofest <dars20@hotmail.com>
2025-09-24 10:25:36 +0200

    Feat: create a class Square that inherits from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Module contains a baseGeometry class"""
+
+
+class BaseGeometry():
+    """Public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """Public instance method that validates the value"""
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle using attributes from  BaseGeometry."""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+
+class Square(Rectangle):
+    """Represents a square using attributes from  BaseGeometry and
+    the subclass Rectangle."""
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
