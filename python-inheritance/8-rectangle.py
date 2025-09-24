8e63c5fd0fafffca4182571a6bf1ac27183ce76a
ofest <dars20@hotmail.com>
2025-09-24 10:23:32 +0200

    Feat: Class base geometry with the area and and an subclass 'rectancle'.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -14,6 +14,7 @@ class BaseGeometry():
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """Represents a rectangle using attributes from  BaseGeometry."""
 
