28584ae27c9f2772ab8f60104f3a73243962afde
ofest <dars20@hotmail.com>
2025-09-25 15:58:22 +0200

    Feat: use abstract base classes to design a blueprint for shapes and use duck typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+
+import math
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """Abstract base class for shapes"""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """Prints area and perimeter of any shape (duck typing)."""
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
