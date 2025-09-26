ca74763b978547c71e8fdf8a799027c21a232a04
ofest <dars20@hotmail.com>
2025-09-26 10:13:10 +0200

    Feat:Create a class named VerboseList that extends the Python list class.

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    """VerboseList that gives notifications on list operations."""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print(f"Extended the list with [{len(iterable)}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item) 
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list.")
+        return item  
