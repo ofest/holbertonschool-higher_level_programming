3bcb9306ef1f068b445c628bcf7b4ad921845799
ofest <dars20@hotmail.com>
2025-09-30 11:30:29 +0200

    Feat: function that appends a string at the end of a text file (UTF8)

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,5 +3,6 @@
 
 
 def append_write(filename="", text=""):
+    """Appends a string at the end of a text file"""
     with open(filename, 'a') as file_object:
         return file_object.write(text)
