95cdab4d7e7da91907b6f45d01da637b8a5ad820
ofest <dars20@hotmail.com>
2025-09-30 15:52:42 +0200

    Feat: Write a class Student that defines differents attributes.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+"""Write a class Student that defines a student by:
+"""
 class Student ():
 
     def __init__(self, first_name, last_name, age):
-
+        """Public instance attributes"""
         self.first_name = first_name
 
         self.last_name = last_name
@@ -11,6 +12,7 @@ class Student ():
         self.age = age
 
     def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student"""
         self.__dict__
         result = {}
         if attrs is None:
