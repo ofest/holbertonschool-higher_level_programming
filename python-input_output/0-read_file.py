201b82c87a73ca76dfa7d91c3996f998d2578f72
ofest <dars20@hotmail.com>
2025-09-30 11:21:53 +0200

    Feat:function that reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ function that reads a text file"""
 
+
 def read_file(filename=""):
+    """reads a text file"""
     with open('my_file_0.txt', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
