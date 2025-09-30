75bedc646146d7c02459ecdd0c6c9140458bea95
ofest <dars20@hotmail.com>
2025-09-30 11:23:45 +0200

    Feat: function that creates an Object from a “JSON file”

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
+"""function that creates an Object from a “JSON file”"""
 
 import json
+
+
 def load_from_json_file(filename):
+    """creates an Object"""
     with open(filename, "r") as file:
-        json_str = file.read ()
+        json_str = file.read()
         return json.loads(json_str)
