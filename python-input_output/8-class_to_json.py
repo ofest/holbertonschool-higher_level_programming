28cb3ec1c4aecbca24722b22651c9f3ce7012d53
ofest <dars20@hotmail.com>
2025-09-30 15:52:34 +0200

    Feat: function that returns the dictionary description with simple data structure

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+"""function that returns the dictionary description with simple data structure """
 
 def class_to_json(obj):
-
+    """returns the dictionary"""
     serialized_dict = {}
 
     for key, value in vars(obj).items():
