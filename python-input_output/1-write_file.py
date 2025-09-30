1f0b68736fc1d1311c00bd6506891c76e1a05cd5
ofest <dars20@hotmail.com>
2025-09-30 11:22:01 +0200

    Feat: function that writes a string to a text file (UTF8)

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ function that writes a string to a text file """
 
+
 def write_file(filename="", text=""):
+    """writes a string"""
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(text)
         return len(text)
