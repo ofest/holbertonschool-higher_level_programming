fb56236e3885e28166dec102196df62edcacb8d5
ofest <dars20@hotmail.com>
2025-10-02 14:58:14 +0200

    Feat: Create a basic serialization module that adds the functionality to serialize

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+
+
 import json
+
 
 def serialize_and_save_to_file(data, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f)
+
 
 def load_and_deserialize(filename):
     with open(filename, "r", encoding="utf-8") as f:
