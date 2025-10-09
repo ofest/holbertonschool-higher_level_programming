9cbf54e94c94e14b3c1c6abd9d6aef92f820c841
ofest <dars20@hotmail.com>
2025-10-09 14:55:59 +0200

    Feat: Set up a basic web server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,22 +1,50 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        
-        # Set response headers
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        
-        # Write response body
-        self.wfile.write(b"Hello, this is a simple API!")
-        self.send_header("Content-type", "application/json")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+
+            json_data = json.dumps(data).encode("utf-8")
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json_data)
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found: The requested URL was \
+                             not found on this server.")
+
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler):
     server_address = ("", 8000)
     httpd = server_class(server_address, handler_class)
-    print("Server running on http://localhost:8000")
+    print ("Server running on http://localhost:8000{}".format(8000))
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
