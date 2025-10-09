1427ab4951086328ea46cb8ae90a541727480aff
ofest <dars20@hotmail.com>
2025-10-09 15:13:29 +0200

    Feat: Set up a basic web server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -25,6 +25,16 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
 
             self.wfile.write(json_data)
 
+        elif self.path == "/info":
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode("utf-8"))
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
