9665bbbec3d37c102b980920b1afb2504040ffa7
ofest <dars20@hotmail.com>
2025-10-10 10:09:36 +0200

    Feat: Set up a basic web server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 
-class SimpleAPIHandler(BaseHTTPRequestHandler):
+class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
@@ -11,19 +12,21 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
             data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
             }
+            self.wfile.write(json.dumps(data).encode(encoding="utf-8"))
 
-            json_data = json.dumps(data).encode("utf-8")
-
+        elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-
-            self.wfile.write(json_data)
+            self.wfile.write(b"OK")
 
         elif self.path == "/info":
             info = {
@@ -35,26 +38,12 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(info).encode("utf-8"))
 
-        elif self.path == "/status":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"OK")
-
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found: The requested URL was \
-                             not found on this server.")
-
-
-def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
-    server_address = ("", port)
-    httpd = server_class(server_address, handler_class)
-    print("Server running on http://localhost:{}".format(port))
-    httpd.serve_forever()
-
+            self.wfile.write(b"Endpoint not found")
 
 if __name__ == "__main__":
-    run()
+    my_serv = HTTPServer(('localhost', 8000), Server)
+    my_serv.serve_forever()
