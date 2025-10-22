9f9a0b4e7a80b319aa7f71721049e82c1d8e3325
ofest <dars20@hotmail.com>
2025-10-22 16:07:39 +0200

    Feat: lists all the cities of California that can be found in the database

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 --  lists all the cities of California that can be found in the database 
-USE hbtn_0d_usa;
+
 SELECT * FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
