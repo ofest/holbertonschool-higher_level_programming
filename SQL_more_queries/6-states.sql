04c7ffb1af1addde228b83705feecd905ffae860
ofest <dars20@hotmail.com>
2025-10-22 16:02:27 +0200

    Feat: Create a data base and a list 'states'

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates a data base 'hbtn_0d_usa' and a table 'states'
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    );
