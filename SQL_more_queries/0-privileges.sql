fdaa55465d69ac7da902b78c93e33ab4af933b14
ofest <dars20@hotmail.com>
2025-10-22 15:13:12 +0200

    Feat: lists all privileges of the MySQL users

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges of the MySQL users user_0d_1 on your server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Lists all privileges of the MySQL users user_0d_2 on your server
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
