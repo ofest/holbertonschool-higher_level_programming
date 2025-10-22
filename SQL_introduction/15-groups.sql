98a99a82b6504f54eadefaeb64855011b629cd84
ofest <dars20@hotmail.com>
2025-10-22 10:49:51 +0200

    Feat: lists the number of records with the same score in the table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
---Lists the number of records with the same score in the table
+-- Lists the number of records with the same score in the table
 SELECT score, COUNT(*) AS number 
 FROM second_table 
 GROUP BY score 
