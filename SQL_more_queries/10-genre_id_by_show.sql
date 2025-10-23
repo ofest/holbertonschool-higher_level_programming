08081994807e7edd443a40d1faefbffa6ba06558
ofest <dars20@hotmail.com>
2025-10-23 15:55:23 +0200

    Feat: List the shows of a database with at least 1 genre.

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- List of shows in a database that have at least 1 genre.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
