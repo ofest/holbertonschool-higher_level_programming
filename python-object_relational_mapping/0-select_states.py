f4a356c46793e0520b830d85df73fb425a9f6367
ofest <dars20@hotmail.com>
2025-10-25 13:58:28 +0200

    Feat: Lists all states from the database hbtn_0e_0_usa.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/venv/bin/python3
+#!/user/bin/python3
 
 """ Lists all states from the database hbtn_0e_0_usa
 """
@@ -6,17 +6,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("SELECT * \
+    FROM states \
+    ORDER BY id ASC")
+    [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
