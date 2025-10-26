6cd3d3854b4114551b63946703e8b79324d48a53
ofest <dars20@hotmail.com>
2025-10-26 23:15:53 +0100

    Contains the class definition of a State and an instance Base

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-from sys import argv
-from model_state import Base, State
+Contains the class definition of a State and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+class State(Base):
+    """State class that links to MySQL table 'states'"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
