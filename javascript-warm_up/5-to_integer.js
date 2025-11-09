ce6ac711484d4673360d166f615c0e4839bbd99a
ofest <dars20@hotmail.com>
2025-11-09 17:48:33 +0100

    Feat: an argument convert to a integer.

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-const num = Number(arg);
-
-if (isNaN(num)) { console.log('Not a number'); } else { console.log('My number: ' + num); }
+const { argv } = require('node:process');
+if (isNaN(Number(argv[2]))) console.log('Not a number');
+else console.log('My number:', Number(argv[2]));
